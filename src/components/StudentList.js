b8f5f4d9e9973245f00f089fc9b820e9178f356c
Taehyuk Chung <tchung60@my.bcit.ca>
2024-02-23 11:42:42 -0800

    modified

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,39 +1,39 @@
-import React from 'react';
-// import students from '../data/students.js.not-used';
-import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import CONSTANTS from '../data/config';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import CONSTANTS from "../data/config";
 
 const StudentList = (param) => {
+  const [studentInfo, setStudentInfo] = useState({});
 
-    const [studentInfo, setStudentInfo] = useState({});
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(
+        `${CONSTANTS.BASE_API_URL}students/`
+      );
+      const body = await result.json();
+      setStudentInfo(body);
+    };
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const result = await fetch(`${CONSTANTS.BASE_API_URL}students/`);
-            const body = await result.json();
-            setStudentInfo(body);
-        }
-        fetchData();
-    }, []);
+  var filteredStudents = studentInfo;
 
-    var filteredStudents = studentInfo;
-
-    if (param !== undefined) {
-        filteredStudents = Object.values(studentInfo).filter(p => p.studentId !== +param.exceptId);
-    }
-
-    return (
-        <React.Fragment>
-            {students.map((student, index) => (
-                <Link key={index} to={`/detail/${student.studentId}`}>
-                    <h6>
-                        {student.studentId} {student.firstName} {student.lastName}
-                    </h6>
-                </Link>
-            ))}
-
-        </React.Fragment>
+  if (param !== undefined) {
+    filteredStudents = Object.values(studentInfo).filter(
+      (p) => p.studentId !== +param.exceptId
     );
-}
+  }
+  return (
+    <React.Fragment>
+      {filteredStudents.map((student, index) => (
+        <Link key={index} to={`/detail/${student.studentId}`}>
+          <h6>
+            {student.studentId} {student.firstName} {student.lastName}
+          </h6>
+        </Link>
+      ))}
+    </React.Fragment>
+  );
+};
 export default StudentList;
